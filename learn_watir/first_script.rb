caf162ee8be0d52ce8bb5902b67d2be6d99d5f9d
Karlo Smid <karlo.smid@gmail.com>
2012-10-10 22:24:08 +0200

    adopting a puppy finished!

--- learn_watir/first_script.rb
+++ learn_watir/first_script.rb
@@ -1,7 +1,15 @@
 require "rubygems"
 require "watir-webdriver"
-iAmFirefox = Watir::Browser.new :firefox
-iAmFirefox.goto 'http://puppies.herokuapp.com'
+iAmFirefox = Watir::Browser.start 'http://puppies.herokuapp.com', :firefox
+iAmFirefox.button(:value => 'View Details', :index => 2).click
+iAmFirefox.button(:value => 'Adopt Me!' ).click
+iAmFirefox.button(:value => 'Complete the Adoption' ).click
+iAmFirefox.text_field(:id => 'order_name').set('Karlo Smid')
+iAmFirefox.text_field(:id => 'order_address').set('Hrusevecka 3\n 10000 Zagreb!')
+iAmFirefox.text_field(:id => 'order_email').set('ides@kam.hr')
+iAmFirefox.select_list(:id => 'order_pay_type').select('Check')
+iAmFirefox.select_list(:id => 'order_pay_type').select('Credit card')
+iAmFirefox.select_list(:id => 'order_pay_type').select('Purchase order')
+iAmFirefox.button(:name => 'commit').click
 sleep 5
 iAmFirefox.close
-
