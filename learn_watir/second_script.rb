6d8c671c08ad490a6d15ec7649689cbd16d20dff
Karlo Smid <karlo.smid@gmail.com>
2012-10-11 21:51:40 +0200

    browser object is now instance variable.

--- learn_watir/second_script.rb
+++ learn_watir/second_script.rb
@@ -2,23 +2,23 @@
 require "rubygems"
 require "watir-webdriver"
 
-def adopt_puppy_by_number_position_on_page_counting_from_top( browser, numberPositionCountingFromTheTopOfPage )
-	browser.button( :value => 'View Details', :index => numberPositionCountingFromTheTopOfPage - 1 ).click
-	browser.button( :value => 'Adopt Me!' ).click
+def adopt_puppy_by_number_position_on_page_counting_from_top( numberPositionCountingFromTheTopOfPage )
+	@iAmFirefox.button( :value => 'View Details', :index => numberPositionCountingFromTheTopOfPage - 1 ).click
+	@iAmFirefox.button( :value => 'Adopt Me!' ).click
 end
 
-iAmFirefox = Watir::Browser.start 'http://puppies.herokuapp.com', :firefox
-adopt_puppy_by_number_position_on_page_counting_from_top( iAmFirefox, 1 )
-iAmFirefox.button(:value => 'Adopt Another Puppy' ).click
-adopt_puppy_by_number_position_on_page_counting_from_top( iAmFirefox, 2 )
-iAmFirefox.button(:value => 'Complete the Adoption' ).click
-iAmFirefox.text_field(:id => 'order_name').set('Karlo Smid')
-iAmFirefox.text_field(:id => 'order_address').set('Hruševečka 3 10000 Zagreb!')
-iAmFirefox.text_field(:id => 'order_email').set('ides@kam.hr')
-iAmFirefox.select_list(:id => 'order_pay_type').select('Credit card')
+@iAmFirefox = Watir::Browser.start 'http://puppies.herokuapp.com', :firefox
+adopt_puppy_by_number_position_on_page_counting_from_top 1
+@iAmFirefox.button(:value => 'Adopt Another Puppy' ).click
+adopt_puppy_by_number_position_on_page_counting_from_top 2
+@iAmFirefox.button(:value => 'Complete the Adoption' ).click
+@iAmFirefox.text_field(:id => 'order_name').set('Karlo Smid')
+@iAmFirefox.text_field(:id => 'order_address').set('Hruševečka 3 10000 Zagreb!')
+@iAmFirefox.text_field(:id => 'order_email').set('ides@kam.hr')
+@iAmFirefox.select_list(:id => 'order_pay_type').select('Credit card')
 sleep 5
-iAmFirefox.button(:name => 'commit').click
+@iAmFirefox.button(:name => 'commit').click
 sleep 4
 fail 'Browser page did not contain "Thank you for adopting a puppy!' \
-unless iAmFirefox.text.include? 'Thank you for adopting a puppy!'
-iAmFirefox.close
+unless @iAmFirefox.text.include? 'Thank you for adopting a puppy!'
+@iAmFirefox.close
